4f6d223c45f4e3484bbd1681266a6b75fa71f968
Lluis Florit <lluis.florit.b@gmail.com>
2021-04-19 00:35:32 +0200

    secret_key switched to enviromental variable

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import os
 from logging import debug
 from flask import Flask, render_template
 from dotenv import load_dotenv
+
+load_dotenv()
+
 from views.alerts import alert_blueprint
 from views.stores import store_blueprint
 from views.users import user_blueprint
+
 
 def create_app():
     app = Flask(__name__)
@@ -23,6 +27,20 @@ def create_app():
     app.register_blueprint(user_blueprint, url_prefix="/users")
 
     return app
+
+# app = Flask(__name__)
+# app.secret_key = os.environ.get("SECRET_KEY") # os.urandom(64)
+# app.config.update(
+#         ADMIN=os.environ.get("ADMIN")
+#     )
+
+# @app.route("/")
+# def home():
+#     return render_template("home.html")
+
+# app.register_blueprint(alert_blueprint, url_prefix="/alerts")
+# app.register_blueprint(store_blueprint, url_prefix="/stores")
+# app.register_blueprint(user_blueprint, url_prefix="/users")
 
     
 if __name__ == "__main__":
