ec1dc55167a942d8396575a41f8636632b215b76
Lluis Florit <lluis.florit.b@gmail.com>
2021-04-19 21:56:59 +0200

    now when you log in you get redirected to home.

--- app.py
+++ app.py
@@ -10,37 +10,37 @@ from views.stores import store_blueprint
 from views.users import user_blueprint
 
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = os.environ.get("SECRET_KEY") # os.urandom(64)
-    load_dotenv()
-    app.config.update(
-        ADMIN=os.environ.get("ADMIN")
-    )
-
-    @app.route("/")
-    def home():
-        return render_template("home.html")
-
-    app.register_blueprint(alert_blueprint, url_prefix="/alerts")
-    app.register_blueprint(store_blueprint, url_prefix="/stores")
-    app.register_blueprint(user_blueprint, url_prefix="/users")
-
-    return app
-
-# app = Flask(__name__)
-# app.secret_key = os.environ.get("SECRET_KEY") # os.urandom(64)
-# app.config.update(
+# def create_app():
+#     app = Flask(__name__)
+#     app.secret_key = os.environ.get("SECRET_KEY") # os.urandom(64)
+#     load_dotenv()
+#     app.config.update(
 #         ADMIN=os.environ.get("ADMIN")
 #     )
 
-# @app.route("/")
-# def home():
-#     return render_template("home.html")
+#     @app.route("/")
+#     def home():
+#         return render_template("home.html")
 
-# app.register_blueprint(alert_blueprint, url_prefix="/alerts")
-# app.register_blueprint(store_blueprint, url_prefix="/stores")
-# app.register_blueprint(user_blueprint, url_prefix="/users")
+#     app.register_blueprint(alert_blueprint, url_prefix="/alerts")
+#     app.register_blueprint(store_blueprint, url_prefix="/stores")
+#     app.register_blueprint(user_blueprint, url_prefix="/users")
+
+#     return app
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") # os.urandom(64)
+app.config.update(
+        ADMIN=os.environ.get("ADMIN")
+    )
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+app.register_blueprint(alert_blueprint, url_prefix="/alerts")
+app.register_blueprint(store_blueprint, url_prefix="/stores")
+app.register_blueprint(user_blueprint, url_prefix="/users")
 
     
 if __name__ == "__main__":
